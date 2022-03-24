f57ff2a0376c0ebb5c9409680a1e0ab24ae83d6b
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-03-24 23:11:51 +0000

    Add stripe

--- checkout/views.py
+++ checkout/views.py
@@ -14,6 +14,8 @@ def checkout(request):
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
+        'stripe_public_key': 'pk_test_51KgzUoBBe3oUHbWGInaYKXUFDDeT7Uf7IvrAat9iiAlDeIXXTp7oVYbfZ2OmX5bkfW4PTZ9YCeYsA6QFllEX4idC00KZ819FMN',
+        'client_secret': 'test client secret',
     }
 
     return render(request, template, context)
