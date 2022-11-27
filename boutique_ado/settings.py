8c5761f2946a1f480a533702a45a3b0ecdca1b95
OlgaJ1989 <olgajasinska89@hotmail.co.uk>
2022-11-27 21:55:50 +0000

    Add build.sh

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -24,6 +24,9 @@ SECRET_KEY = os.environ.get('SECRET_KEY', '')
 DEBUG = 'DEVELOPMENT' in os.environ
 
 ALLOWED_HOSTS = ['boutique-ado-2022.herokuapp.com', 'localhost']
+RENDER_EXTERNAL_HOSTNAME = os.environ.get('RENDER_EXTERNAL_HOSTNAME')
+if RENDER_EXTERNAL_HOSTNAME: 
+    ALLOWED_HOSTS.append(RENDER_EXTERNAL_HOSTNAME)
 
 
 # Application definition
